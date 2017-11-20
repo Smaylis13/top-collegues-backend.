da8009ca86af500a00239449df3adc35894e6ebe
Ismail GACHA <igacha664@gmail.com>
2017-11-20 13:31:38 +0100

    maj

--- src/main/java/com/example/topcolleguesbackend/controller/CollegueController.java
+++ src/main/java/com/example/topcolleguesbackend/controller/CollegueController.java
@@ -20,7 +20,7 @@ import com.example.topcolleguesbackend.repository.CollegueRepository;
 
 @RestController
 @RequestMapping("/collegues")
-@CrossOrigin(origins = {"http://localhost:4200", "*"})
+@CrossOrigin(origins = "*")
 public class CollegueController {
 	/** colRep : CollegueRepository */
 	@Autowired
